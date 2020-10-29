7d994444275c43076acfeeaccf62b01465c42f47
kim-mini <kimminyi22@gmail.com>
2020-10-29 16:21:06 +0000

    '1030'

--- 201028.c
+++ 201028.c
@@ -18,14 +18,13 @@ int main(int argc, char ** argv)
 void StartGame(void) {//게임의 몸통이 되는 함수
 	int len = 0;
 	char* gameAnswer;
+	char* inputAnswer;
 
 	puts("==========Game Start!==========\n");
 
 	len = choiceLevel();
 	gameAnswer = makeAnswer(len);
-	for (int i = 0; i < len; i++) {
-		printf("%d", gameAnswer[i]);
-	}
+	inputAnswer = userInput(len);
 }
 
 int choiceLevel(void) { //게임의 레벨을 입력받아 정할 수 있도록 하는 함수
@@ -51,7 +50,7 @@ char* makeAnswer(int len) { //게임의 렌덤 정답 값을 만들어내는 함
 		Answer[i] = (char)(rand() % 10 + (int)'0');
 	//	printf("%c", Answer[i]);
 
-		if (i = 0){
+		if (i == 0){
 			if(Answer[i] == '0') {
 				i--;
 				continue;
@@ -75,15 +74,51 @@ char* makeAnswer(int len) { //게임의 렌덤 정답 값을 만들어내는 함
 	return Answer; //힙영역의 메모리의 주소값을 리턴한다.
 }
 
-int userInput(int len) { //유저가 숫자를 입력하게 한다.
+char* userInput(int len) { //유저가 숫자를 입력하게 한다.
+		int check = 0;
+
 		char* userAnswer = (char*)malloc(sizeof(char)*len);
 		memset(userAnswer, 0,sizeof(char)*len);
-
-		printf("숫자 %d개를 입력해주세요", len);
-		scanf(userAnswer);
 		
-		for(int i = 0; i < len; i ++){
-			if(userAnswer[i] == )
+		while(1){
+			printf("숫자 %d개를 입력해주세요", len);
+			scanf("%s", userAnswer);
+			check = answerCheck(userAnswer, len);
+			
+			if(check == -1){//입력받은 값이 첫번째자리가 0이거나 겹치는 숫자가 있으면 -1을 리턴한다.
+				memset(userAnswer,0,sizeof(char)*len);
+				continue;// -1이라면 다시 값을 받도록한다
+			}
+			else{
+				break;
+			}
 		}
+		return userAnswer;
 }
 
+int answerCheck(char* user, int len){//입력받는 값을 체크해준다
+	int returnNum = 0;
+
+	for(int i = 0; i < len; i ++){
+		int faile = 0;
+
+		if(i == 0){
+			if(user[i] == '0'){
+				faile++;
+			}
+		}
+		else {
+			for(int j = 0; j < i; j++){
+				if(user[i] == user[j]){
+					faile++;
+				}
+			}
+		}
+		if(faile == 1){
+			returnNum = -1;
+			break;
+		}
+		break;
+	}
+	return returnNum;
+}
