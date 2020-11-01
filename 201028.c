dc8127383f23e47e5985b0da617a40b3f2cf32e2
kim-mini <kimminyi22@gmail.com>
2020-11-01 09:29:02 +0000

    '1030'

--- 201028.c
+++ 201028.c
@@ -26,7 +26,10 @@ void StartGame(void) {//게임의 몸통이 되는 함수
 	len = choiceLevel();
 	gameAnswer = makeAnswer(len);
 	inputAnswer = userInput(len);
+<<<<<<< HEAD
 	compareAnswer(gameAnswer, inputAnswe);
+=======
+>>>>>>> 7d99444... '1030'
 }
 
 int choiceLevel(void) { //게임의 레벨을 입력받아 정할 수 있도록 하는 함수
@@ -85,7 +88,11 @@ char* userInput(int len) { //유저가 숫자를 입력하게 한다.
 		while(1){
 			printf("숫자 %d개를 입력해주세요", len);
 			scanf("%s", userAnswer);
+<<<<<<< HEAD
 			check = checkInput(userAnswer, len);
+=======
+			check = answerCheck(userAnswer, len);
+>>>>>>> 7d99444... '1030'
 			
 			if(check == -1){//입력받은 값이 첫번째자리가 0이거나 겹치는 숫자가 있으면 -1을 리턴한다.
 				memset(userAnswer,0,sizeof(char)*len);
@@ -98,7 +105,11 @@ char* userInput(int len) { //유저가 숫자를 입력하게 한다.
 		return userAnswer;
 }
 
+<<<<<<< HEAD
 checkInput(char* user, int len){//입력받는 값을 체크해준다
+=======
+int answerCheck(char* user, int len){//입력받는 값을 체크해준다
+>>>>>>> 7d99444... '1030'
 	int returnNum = 0;
 
 	for(int i = 0; i < len; i ++){
@@ -124,6 +135,7 @@ checkInput(char* user, int len){//입력받는 값을 체크해준다
 	}
 	return returnNum;
 }
+<<<<<<< HEAD
 
 void compareAnswer(char *Ans, char *inpans, int len){//유저에게 입력받은 숫자와 게임의 정답 비교
 	int strike = 0;
@@ -148,3 +160,5 @@ void compareAnswer(char *Ans, char *inpans, int len){//유저에게 입력받은
 
 	printf("[%s] '%d'strike / '%d'ball", Ans, strike, ball);
 }	
+=======
+>>>>>>> 7d99444... '1030'
