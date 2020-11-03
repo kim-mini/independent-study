6889c58a7c410db61e482c34c55f2529b3733c40
kim-mini <72716397+kim-mini@users.noreply.github.com>
2020-11-03 19:35:20 +0900

    Update 201013.c

--- 201013.c
+++ 201013.c
@@ -6,19 +6,19 @@ main(void)
 {
     int n = 0;
     int ans = 0;
-    
-    printf("정수를 입력해주세요");
-    scanf("%d",&N);
-    get_answer(n);
-    printf("%d의 약수는 %d입니다.", N,ans);
+
+    printf("정수를 입력해주세요 : ");
+    scanf("%d", &n);
+    ans = get_answer(n);
+    printf("%d의 약수들의 합은 %d입니다.", n, ans);
     return 0;
 }
 
 
 int get_answer(int n) {// 약수구하기
     int answer = 0;
-    for(int i = 1; i <= n; i++){
-        if(n%i == 0){
+    for (int i = 1; i <= n; i++) {
+        if (n % i == 0) {
             answer += i;
         }
     }
