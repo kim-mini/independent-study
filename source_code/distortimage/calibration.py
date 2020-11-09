c398cef12af2569bf7b2eacede56a36ad8c11af8
kim-mini <72716397+kim-mini@users.noreply.github.com>
2020-11-09 19:15:48 +0900

    Update calibration.py

--- source_code/distortimage/calibration.py
+++ source_code/distortimage/calibration.py
@@ -13,14 +13,13 @@ objpoints = []  # 3D points in real world space
 imgpoints = []  # 2D points in image plane
 
 def calib():
+    #어느정도 반복을 할 지 정해주는 기준
     criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
-    """
-    To get an undistorted image, we need camera matrix & distortion coefficient
-    Calculate them with 9*6 20 chessboard images
-    """
+    
     # Prepare object points
     objp = np.zeros((9 * 7, 3), np.float32)
-
+    
+    #mgrid[]는 반대로 출력이 되기때문에 반대로 입력을 해주어야 한다 
     objp[:, :2] = np.mgrid[0:7, 0:9].T.reshape(-1, 2)  # x,y coordinates
 
     for fname in images:
@@ -30,14 +29,10 @@ def calib():
         # Convert to grayscale
         gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-
-
-        # Find the chessboard corners
+        # 체스판의 코너찾기
         ret, corners = cv2.findChessboardCorners(gray, (7, 9), None)
 
-
-
-        # If corners are found, add object points, image points
+        # 찾았으면 objpoints, imgpoints에 추가하
         if ret == True:
 
             objpoints.append(objp)
@@ -45,11 +40,11 @@ def calib():
             imgpoints.append(corners)
 
             img = cv2.drawChessboardCorners(img,(7,6),corners2,ret)
-            #cv2.imshow('img', img)
+            cv2.imshow('img', img)
             cv2.waitKey()
 
         else:
-            continue
+            continue기
 
     cv2.destroyAllWindows()
 
